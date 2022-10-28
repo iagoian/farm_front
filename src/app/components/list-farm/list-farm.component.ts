1fc0fcd9bc842ca92144c14a960a917e61f72f59
iago ian <iagoian23@hotmail.com>
2022-10-28 11:55:00 -0400

    implementação da listagem

--- src/app/components/list-farm/list-farm.component.ts
+++ src/app/components/list-farm/list-farm.component.ts
@@ -10,18 +10,45 @@ import { FarmService } from 'src/app/services/farm.service';
 })
 export class ListFarmComponent implements OnInit {
 
-  farms: Farm[] = [];
+  farms: Farm = new Farm();
+  farmsArr: Array<Farm> = new Array<Farm>();
 
   constructor(
     private farmeService: FarmService,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.farms = this.farmeService.list();
+    this.getfarms()
+  };
+
+  getfarms(){
+    this.farmeService.listAll().subscribe(response => {
+      for(let i of response){
+        this.farms.name = i.name;
+        this.farms.area = i.area;
+        this.farms.centroid = i.centroid;
+        this.farms.geometry = i.geometry;
+        this.farms.owner = i.owner;
+      }
+      this.farmsArr.push(this.farms)
+      console.log(this.farmsArr)
+    })
   }
 
-  nav(){
+  listfarm(): string[] {
+    return ['name', 'geometry', 'area', 'centroid', 'owner'];
+  }
+
+  // delete(farm: number) {
+  //   for(let i=0; i<=this.farmsArr.length-1; i++){
+  //     if(this.farmsArr[i].id == farm){
+  //       this.farmsArr.splice(i,1)
+  //     }
+  //   }
+  // }
+
+  nav() {
     this.router.navigateByUrl("components/register-farm");
   }
 }
