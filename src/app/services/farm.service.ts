086d1e782f158f21e5a1747776e2c4e57679d542
iago ian <iagoian23@hotmail.com>
2022-10-28 11:57:22 -0400

    atualização do service

--- src/app/services/farm.service.ts
+++ src/app/services/farm.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Farm } from '../models/Farm';
 import { Owner } from '../models/Owner';
-import { Farm } from './../models/Farm';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,79 +16,38 @@ const httpOptions = {
 
 export class FarmService {
 
-  url=`${environment.apiBaseUrl}/farms`;  
+url=`${environment.apiBaseUrl}/farms`;  
 
   constructor(private http: HttpClient){
 
   }
 
-  public create(farms: Farm) {
-    const apiUrl=`${this.url}/create`;
-    return this.http.post<Farm>(apiUrl,farms);
-  }
-  
-
-  public read(id: number): Farm {
-
-    return {} as any
-  }
-
-  public list(): any {
-    const apiUrl = `${this.url}/`;
+  listAll(): any {
+    const apiUrl = `${this.url}`;
     return this.http.get<any>(apiUrl);
   }
 
-  public update(farm: Farm){
+  register(farms: Farm) {
+    const apiUrl=`${this.url}/farms`;
+    return this.http.post<Farm>(apiUrl,farms);
+  }
+  
+  read(id: number): Owner {
+
+    return this.listAll().find((owner:  Owner) => owner.id === id);
+  }
+
+
+  update(farm: Farm){
     const apiUrl = `${this.url}/update`;
     return this.http.put<Farm>(apiUrl,farm);
 
   }
 
-  public delete(owner_id: number){
-    const apiUrl = `${this.url}/apagar/${owner_id = 1}`;
+  delete(owner_id: number){
+    const apiUrl = `${this.url}/delete/${owner_id}`;
     return this.http.delete<Farm>(apiUrl);
 
   }
   
 }
-
-// Service localStorage
-// export class farmService {
-
-//   constructor() { }
-
-//   listAll(): Farm[] | any[] {
-//       const farms = localStorage.getItem('farms');
-//       return farms ? JSON.parse(farms) : [];
-//   }
-
-//   register(farm: Farm): void {
-//       farm.id = new Date().getTime();
-//       const farms = this.listAll();
-//       farms.push(farm);
-//       localStorage.setItem('farms', JSON.stringify(farms));
-//   }
-
-//   buscarPorId(id: number): Farm {
-//       return this.listAll().find((farm:  Farm) => farm.id === id);
-//   }
-
-//   update(farm: Farm): void {
-//       const farms: Farm[] = this.listAll();
-//       farms.forEach((element, key) => {
-//           if (farm.id === element.id) {
-//               farms[key] = farm;
-//           }
-//           if (key === farms.length -1) {
-//               localStorage.setItem('farms', JSON.stringify(farms));
-//           }
-//       });
-//   }
-
-//   remover(id: number): void {
-//       let farms: farm[] = this.listAll();
-//       farms = farms.filter(farm => farm.id !== id);
-//       localStorage['farms'] = JSON.stringify(farms);
-//   }
-
-// }
