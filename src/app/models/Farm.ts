8f0e212c7a85eb91498d60676a4036bc407af4ec
iago ian <iagoian23@hotmail.com>
2022-10-28 11:50:00 -0400

    atualização do model Farm e Owner

--- src/app/models/Farm.ts
+++ src/app/models/Farm.ts
@@ -1,12 +1,11 @@
 import { Owner } from './Owner'
-export interface Farm {
-  setItem(arg0: any, objJson: string): unknown
-  id: number
-  name: string
-  geometry: any
-  area: number
-  centroid: number[]
+export class Farm {
+  name?: string
+  id?: number
+  geometry?: any
+  area?: number
+  centroid?: number[]
   creation_date?: Date
-  owner: Owner
-
+  owner: any
+  
 }
