d9d554ad8a57b4e1c5df7cf35fcd915f76496c67
iago ian <iagoian23@hotmail.com>
2022-10-28 11:55:38 -0400

    implementação do registro

--- src/app/components/register-farm/register-farm.component.ts
+++ src/app/components/register-farm/register-farm.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Owner } from 'src/app/models/Owner';
 import { Farm } from 'src/app/models/Farm';
+import { FarmService } from 'src/app/services/farm.service';
 
 @Component({
   selector: 'app-register-farm',
@@ -8,19 +10,26 @@ import { Farm } from 'src/app/models/Farm';
   styleUrls: ['./register-farm.component.scss']
 })
 export class RegisterFarmComponent implements OnInit {
-
   
-  constructor() { }
+  public farm: Farm = {
+    name: '',
+    geometry: undefined,
+    area: 0,
+    centroid: [],
+    owner: new Owner(),
+  };
+  
+  constructor(
+    private farmService: FarmService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     
   }
 
-  salvarDados(){
-    // this.route.navigate(['/list'])
+  createfarm(farm: FarmService) {
+    this.farmService.register(this.farm);
+    this.router.navigateByUrl('/list-farm');
   }
-  ownerId() {
-    throw new Error('Method not implemented.');
-  }
-
 }
